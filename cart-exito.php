385b67495310894053e7c82220ac8d52f8774fd8
Max Manuel <maxcamina@gmail.com>
2024-12-22 21:39:47 -0500

    pedido realizado con exito

---
+++ cart-exito.php
@@ -0,0 +1,140 @@
+<?php include("template-parts/header.php") ?>
+
+<section class="breadcrumb-area bg-primary-gradient">
+  <div class="container">
+    <div class="breadcrumb-content text-center">
+      <h2 class="mb-3">Carrito</h2>
+      <p>Nuestro equipo de profesionales TI está a tu disposición. Siempre listos y en tu idioma,<br> estamos para ayudarte con cualquier servicio que hayas adquirido.</p>
+    </div>
+  </div>
+</section>
+
+<div class="wrapper-cart">
+  <div class="sc-hoHwyw gvQPCZ">
+    <section data-testid="step-progress" class="sc-dogTuw iMAsDD">
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <div class="sc-lcLUZz iOPvCF">
+              <div title="Carrito" class="sc-dlUKyu btrwjN">
+                <svg width="36" height="36" data-testid="progress-circle">
+                  <circle cx="50%" cy="50%" r="15.5" stroke-width="5" class="sc-iSLKLn hblLnf"></circle>
+                  <circle cx="50%" cy="50%" r="15.5" stroke-width="5" stroke-dasharray="97.38937226128358" stroke-dashoffset="0" class="sc-bbUAki bJOIDK"></circle>
+                  <image data-testid="progress-circle-fixed-step" width="18" height="18" xlink:href="assets/img/imagenes-inkaweb/icon-cart.svg" x="26%" y="26%"></image>
+                </svg>
+                <div class="sc-krduYZ djGVnW">
+                  <p class="sc-cAmkxU jcQmwj mb-0">Elegir Producto</p>
+                </div>
+              </div>
+              <div title="Identificación" class="sc-dlUKyu btrwjN"><svg width="36" height="36" data-testid="progress-circle">
+                  <circle cx="50%" cy="50%" r="15.5" stroke-width="5" class="sc-iSLKLn hblLnf"></circle>
+                  <circle cx="50%" cy="50%" r="15.5" stroke-width="5" stroke-dasharray="97.38937226128358" stroke-dashoffset="97.38937226128358" class="sc-bbUAki bJOIDK"></circle><text data-testid="progress-circle-text" x="50%" y="60%" class="sc-leYdVB cQevNy">2</text>;
+                </svg>
+                <div class="sc-krduYZ djGVnW">
+                  <p class="sc-cAmkxU jTHeQM mb-0">Opciones de Dominio</p>
+                </div>
+              </div>
+              <div title="Pago" class="sc-dlUKyu btrwjN"><svg width="36" height="36" data-testid="progress-circle">
+                  <circle cx="50%" cy="50%" r="15.5" stroke-width="5" class="sc-iSLKLn hblLnf"></circle>
+                  <circle cx="50%" cy="50%" r="15.5" stroke-width="5" stroke-dasharray="97.38937226128358" stroke-dashoffset="97.38937226128358" class="sc-bbUAki bJOIDK"></circle><text data-testid="progress-circle-text" x="50%" y="60%" class="sc-leYdVB cQevNy">3</text>;
+                </svg>
+                <div class="sc-krduYZ djGVnW">
+                  <p class="sc-cAmkxU jTHeQM mb-0">Configurar</p>
+                </div>
+              </div>
+              <div title="Confirmación" class="sc-dlUKyu btrwjN"><svg width="36" height="36" data-testid="progress-circle">
+                  <circle cx="50%" cy="50%" r="15.5" stroke-width="5" class="sc-iSLKLn hblLnf"></circle>
+                  <circle cx="50%" cy="50%" r="15.5" stroke-width="5" stroke-dasharray="97.38937226128358" stroke-dashoffset="97.38937226128358" class="sc-bbUAki bJOIDK"></circle><text data-testid="progress-circle-text" x="50%" y="60%" class="sc-leYdVB cQevNy">4</text>;
+                </svg>
+                <div class="sc-krduYZ djGVnW">
+                  <p class="sc-cAmkxU jTHeQM mb-0">Confirmar Pedido</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+
+  <main class="sc-ekrjqK jDByLM">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="sc-bttaWv jUSKdL">
+            <div data-testid="empty" class="sc-fIITEw eghotX">
+              <div class="sc-gFkHhu kiKcrx">
+                <svg enable-background="new 0 0 512 512" viewBox="0 0 512 512" width="64" xmlns="http://www.w3.org/2000/svg">
+                  <switch>
+                    <foreignObject height="1" requiredExtensions="http://ns.adobe.com/AdobeIllustrator/10.0/" width="1" />
+                    <g>
+                      <g id="_x30_1_Logout" opacity=".93" />
+                      <g id="_x30_2_User" />
+                      <g id="_x30_3_Login" />
+                      <g id="_x30_4_Close" />
+                      <g id="_x30_5_Accept" />
+                      <g id="_x30_6_Heart" />
+                      <g id="_x30_7_Shopping_Bag" />
+                      <g id="_x30_8_Cursor" />
+                      <g id="_x30_9_Settings" />
+                      <g id="_x31_0_Location" />
+                      <g id="_x31_1_Star" />
+                      <g id="_x31_2_Cloud" />
+                      <g id="_x31_3_Plus" />
+                      <g id="_x31_4_Search" />
+                      <g id="_x31_5_Chat" />
+                      <g id="_x31_6_Success">
+                        <g fill="#bcf4ab">
+                          <path d="m309.4 195.7-81.6 81.6-27.1-27.1c-9.8-9.8-25.6-9.8-35.4 0s-9.8 25.6 0 35.4l44.8 44.8c4.7 4.7 11 7.3 17.7 7.3s13-2.6 17.7-7.3l99.3-99.3c9.8-9.8 9.8-25.6 0-35.4-9.9-9.8-25.7-9.8-35.4 0z" />
+                          <path d="m507.6 313.3-39.1-57.3 39.2-57.3c4.4-6.4 5.5-14.4 3.2-21.8-2.4-7.4-8-13.2-15.2-15.8l-68.1-24.8-3.7-68.6c-.4-7.5-4.1-14.4-10.2-18.8-6-4.4-13.7-5.9-21-4.1l-71.3 18.2-46.4-54.2c-4.7-5.6-11.7-8.8-19-8.8s-14.3 3.2-19 8.8l-46.2 54.3-71.3-18.2c-7.2-1.9-15-.4-21 4.1-6 4.4-9.8 11.3-10.2 18.8l-3.7 68.6-68.1 24.8c-7.3 2.6-12.9 8.5-15.2 15.8-2.4 7.4-1.2 15.4 3.2 21.8l39 57.2-39.1 57.3c-4.4 6.3-5.6 14.4-3.2 21.7 2.4 7.4 8 13.2 15.2 15.8l68.1 24.8 3.7 68.6c.4 7.5 4.1 14.4 10.2 18.8 6 4.4 13.7 5.9 21 4.1l71.3-18.2 46.2 54.3c4.8 5.6 11.7 8.8 19 8.8s14.3-3.2 19-8.8l46.2-54.3 71.3 18.2c7.3 1.9 15 .4 21-4.1 6-4.4 9.8-11.3 10.2-18.8l4.6-85c.7-13.8-9.8-25.6-23.6-26.3-13.8-.8-25.6 9.8-26.3 23.6l-3 54.6-57-14.6c-9.3-2.4-19.1.8-25.2 8l-37.1 43.9-37.2-43.8c-6.2-7.3-16-10.4-25.2-8l-57 14.6-3-54.6c-.5-10-7-18.7-16.4-22.1l-53.4-19.4 30.6-44.8c5.8-8.5 5.8-19.7 0-28.2l-30.6-44.8 53.4-19.4c9.4-3.4 15.9-12.1 16.4-22.1l3-54.6 57 14.6c9.3 2.4 19.1-.7 25.2-8l37.2-44.2 37.2 43.8c6.2 7.3 16 10.4 25.2 8l57-14.6 3 54.6c.5 10 7 18.7 16.4 22.1l53.4 19.4-30.6 44.8c-5.8 8.5-5.8 19.7 0 28.2l48.8 71.4c7.8 11.4 23.4 14.3 34.7 6.5 11.4-7.6 14.3-23.1 6.5-34.5z" />
+                        </g>
+                      </g>
+                      <g id="_x31_7_Download" />
+                      <g id="_x31_8_Pencil" />
+                      <g id="_x31_9_Chevron" />
+                      <g id="_x32_0_User" />
+                      <g id="_x32_1_Minus" />
+                      <g id="_x32_2_Loading" />
+                      <g id="_x32_3_Upload" />
+                      <g id="_x32_4_Password" />
+                      <g id="_x32_5_Info" />
+                      <g id="_x32_6_Settings" />
+                      <g id="_x32_7_Chevron" />
+                      <g id="_x32_8_Menu" />
+                      <g id="_x32_9_Share" />
+                      <g id="_x33_0_Chevron" />
+                      <g id="_x33_1_Smartphone" />
+                      <g id="_x33_2_Link" />
+                      <g id="_x33_3_Folder" />
+                      <g id="_x33_4_Checked" />
+                      <g id="_x33_5_Bookmark" />
+                      <g id="_x33_6_Filter" />
+                      <g id="_x33_7_Cloud_Computing" />
+                      <g id="_x33_8_Question" />
+                      <g id="_x33_9_Eye" />
+                      <g id="_x34_0_Zoom_In" />
+                      <g id="_x34_1_Zoom_Out" />
+                      <g id="_x34_2_Trash" />
+                      <g id="_x34_3_Trash_Bin" />
+                      <g id="_x34_4_Send" />
+                      <g id="_x34_5_Notification" />
+                      <g id="_x34_6_Phone_Call" />
+                      <g id="_x34_7_Email" />
+                      <g id="_x34_8_Shopping_Cart" />
+                      <g id="_x34_9_Camera" />
+                      <g id="_x35_0_Menu" />
+                    </g>
+                  </switch>
+                </svg>
+                <p class="sc-dRXgYj bSWqwn">¡Pedido realizado con éxito!</p>
+                <p class="sc-dDgCVK QBWEe">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                <a href="" class="sc-gtLQVN fgiAih"><button aria-label="Default aria-label" data-testid="button-empty" title="Ir al sitio web" type="button" class="sc-eTwdGJ kGysuc">Ver boleta de Pago</button></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</div>
+<?php include("template-parts/footer.php") ?>
